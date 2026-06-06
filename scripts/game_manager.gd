29c989cb4e0af9b4a5f605c5b513b23ebf26e44b
Vlad <vldmirea@gmail.com>
2026-06-06 16:31:23 -0400

    test initial screen changing logic

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,20 +6,31 @@ extends Node
 @export var buyingScene: PackedScene
 
 enum State {MAIN_MENU, FISHING, SELLING, BUYING}
-var current_state = State.MAIN_MENU
+var initial_state = State.MAIN_MENU
+var current_screen: Node
 var state_machine: StateMachine
+
+var valid_transitions = {
+	State.MAIN_MENU: [State.FISHING],
+	State.FISHING: [State.SELLING, State.MAIN_MENU],
+	State.SELLING: [State.BUYING, State.MAIN_MENU],
+	State.BUYING: [State.FISHING, State.MAIN_MENU]
+}
+	
+var state_scenes: Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var valid_transitions = {
-		State.MAIN_MENU: [State.FISHING],
-		State.FISHING: [State.SELLING, State.MAIN_MENU],
-		State.SELLING: [State.BUYING, State.MAIN_MENU],
-		State.BUYING: [State.FISHING, State.MAIN_MENU]
+	state_scenes = {
+		State.MAIN_MENU: mainMenuScene,
+		State.FISHING: fishingScene,
+		State.SELLING: sellingScene,
+		State.BUYING: buyingScene
 	}
+
 	state_machine = StateMachine.new(valid_transitions)
 	state_machine.state_changed.connect(_on_state_changed)
-	state_machine.change_state(current_state)
+	state_machine.change_state(initial_state)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,4 +39,20 @@ func _process(delta):
 
 
 func _on_state_changed(from: int, to: int, context: Dictionary) -> void:
-	pass
+	change_screen(to)
+	
+func _on_action_requested(action: String):
+	match action:
+		"play":
+			state_machine.change_state(State.FISHING)
+		"main_menu":
+			state_machine.change_state(State.MAIN_MENU)			
+			
+func change_screen(state: int):
+	if current_screen:
+		current_screen.queue_free()
+	
+	current_screen = state_scenes[state].instantiate()
+	add_child(current_screen)
+	
+	current_screen.action_requested.connect(_on_action_requested)
