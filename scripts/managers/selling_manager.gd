e590023ff11eec674dd900bd7fad2fc30a87d017
Daniel Yoo <daniel.yoo0523@gmail.com>
2026-06-07 18:02:26 -0400

    added pause menu and functionality to both mobile and pc

--- scripts/managers/selling_manager.gd
+++ scripts/managers/selling_manager.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 signal transition_requested(state: Global.State)
 
+@export var pause_button: Button
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pause_button.pressed.connect(_on_button_pause_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,4 +22,7 @@ func _on_button_buy_pressed():
 
 func _on_request_spawn_customer(customer: Customer):
 	add_child(customer)
+
+func _on_button_pause_pressed():
+	PauseMenu.toggle()
 	
