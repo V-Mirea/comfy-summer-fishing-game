13a1a917ad252c85cf831f155b1a57285f06d625
Daniel Yoo <daniel.yoo0523@gmail.com>
2026-06-06 21:49:56 -0400

    added new fish data organization

---
+++ scripts/models/fish_species.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+#the base fish, we'll be using to pull data from
+class_name FishSpecies
+
+@export var display_name: String = ""
+@export var base_price: int = 0
+@export var pattern: Array[Vector2] = []
