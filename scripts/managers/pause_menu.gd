919574e9a65b392d6cf1fdac81e14553a013bcd8
bowlofbap <daniel.yoo0523@gmail.com>
2026-06-07 22:52:21 -0400

    Feature/dy/select selling fish (#23)
    * reformatted the fish to be a resource, and removed fish inventory from glboals file
    
    * initial player data schaffolding
    
    * refactored fishing game to read and hold actual fish creataed during roll process
    
    * added pause menu and functionality to both mobile and pc
    
    * made changes to have a selling manager inbetween fishing and selling

---
+++ scripts/managers/pause_menu.gd
@@ -0,0 +1,58 @@
+extends CanvasLayer
+
+@export var money_label: Label
+@export var upgrades_list: VBoxContainer
+@export var fish_list: VBoxContainer
+@export var resume_button: Button
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	visible = false
+	resume_button.pressed.connect(toggle)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		toggle()
+		get_viewport().set_input_as_handled()
+
+func toggle() -> void:
+	if not visible:
+		open()
+	else:
+		close()
+
+func open() -> void:
+	_refresh()
+	visible = true
+	get_tree().paused = true
+
+func close() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _refresh() -> void:
+	money_label.text = "Money: %d" % PlayerManager.data.money
+
+	#clear and rebuild upgrades, iterating from the player manager data. we could make it reactive, but note that 
+	# on first opening from save data, we would need to rebuild anyways. so i believe most things in the pause menu should be 
+	# not reactive
+	for child in upgrades_list.get_children():
+		child.queue_free()
+	for upgrade_id in PlayerManager.data.upgrades:
+		var label := Label.new()
+		label.text = "%s: Lv %d" % [upgrade_id.capitalize(), PlayerManager.data.upgrades[upgrade_id]]
+		upgrades_list.add_child(label)
+
+	#same goes for fish
+	for child in fish_list.get_children():
+		child.queue_free()
+	var fish_inventory := PlayerManager.get_all_fish()
+	if fish_inventory.is_empty():
+		var label := Label.new()
+		label.text = "No fish"
+		fish_list.add_child(label)
+	else:
+		for fish in fish_inventory:
+			var label := Label.new()
+			label.text = "%s (Quality: %d)" % [fish.species.display_name, fish.quality]
+			fish_list.add_child(label)
