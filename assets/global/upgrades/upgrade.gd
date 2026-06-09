0b3212b04ec8a4f5293ed3b9c891ae303e62ace0
SpeedOfSmell <vldmirea@gmail.com>
2026-06-09 16:26:29 -0400

    Create upgrade database and add a couple rudimentary upgrades

---
+++ assets/global/upgrades/upgrade.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name Upgrade
+
+enum UpgradeType { ROD, REEL, LINE, BAIT, SHOP_LEVEL, ADVERTISING, POLLUTION, CONSERVATION }
+
+@export var type: UpgradeType
+@export_range(0, 10) var level: int
+@export_range(0, 1000, 1, "or_greater") var price: int
+
+@export var name: String
+@export var description: String
+#@export var sprite
---
+++ assets/global/upgrades/upgrade_database.gd
@@ -0,0 +1,6 @@
+extends Node
+
+# upgrades should start at level 0 so that they match their array index and allows for easy indexing
+@export var rod_upgrades: Array[Upgrade]
+@export var reel_upgrades: Array[Upgrade]
+@export var bait_upgrades: Array[Upgrade]
