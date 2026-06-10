9acc4a8fd536cfd2bf05b17db7e14b1d802eb887
SpeedOfSmell <vldmirea@gmail.com>
2026-06-10 13:52:29 -0400

    let user close dialogue with esc

--- assets/game/buying_scene/dialogue_window.gd
+++ assets/game/buying_scene/dialogue_window.gd
@@ -9,13 +9,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func close_window():
+	get_tree().paused = false
+	visible = false
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel") and visible:
+		get_viewport().set_input_as_handled()
+		close_window()
 
+func _on_exit_button_pressed():
+	close_window()
 
 func _on_open_dialogue_box():
 	get_tree().paused = true
 	visible = true
-
-
-func _on_exit_button_pressed():
-	get_tree().paused = false
-	visible = false
