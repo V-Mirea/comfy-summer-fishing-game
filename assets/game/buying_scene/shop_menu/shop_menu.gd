ab05e09ec2afec13e3f0b722140b3669b84c0838
SpeedOfSmell <vldmirea@gmail.com>
2026-06-10 00:17:00 -0400

    fix background and update shop menu when an item is purchased

--- assets/game/buying_scene/shop_menu/shop_menu.gd
+++ assets/game/buying_scene/shop_menu/shop_menu.gd
@@ -5,10 +5,13 @@ extends CanvasLayer
 @export var purchase_button: Button
 @export var upgrade_button_scene: PackedScene
 
+var selected_upgrade: Upgrade
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+# BUG: the buttons are being pushed off the menu when reopening the shop 
 func populate_shop(upgrades_sold: Upgrade.UpgradeType):
 	var button_group: ButtonGroup = ButtonGroup.new()
 	for upgrade: Upgrade in UpgradeDatabase.upgrades_dictionary[upgrades_sold]:
@@ -16,16 +19,8 @@ func populate_shop(upgrades_sold: Upgrade.UpgradeType):
 		upgrade_button.setup(upgrade, button_group)
 		upgrade_button.upgrade_selected.connect(_on_upgrade_selected)
 		items_container.add_child(upgrade_button)
-
-func _on_shop_opened(upgrades_sold: Upgrade.UpgradeType):
-	get_tree().paused = true
-	visible = true
-	
-	populate_shop(upgrades_sold)
-	
-func _on_upgrade_selected(selected_upgrade: Upgrade):
-	description_label.text = selected_upgrade.description
-	
+		
+func update_locked_and_purchased_items():
 	var player_upgrade_level = PlayerManager.get_upgrade_level(selected_upgrade.type)
 	if player_upgrade_level > selected_upgrade.level - 1:
 		purchase_button.disabled = true
@@ -37,8 +32,24 @@ func _on_upgrade_selected(selected_upgrade: Upgrade):
 		purchase_button.disabled = false
 		purchase_button.text = "Purchase"
 
+func _on_shop_opened(upgrades_sold: Upgrade.UpgradeType):
+	get_tree().paused = true
+	visible = true
+	
+	populate_shop(upgrades_sold)
+	
+func _on_upgrade_selected(selected_upgrade: Upgrade):
+	self.selected_upgrade = selected_upgrade
+	description_label.text = selected_upgrade.description
+	
+	update_locked_and_purchased_items()
+
 func _on_purchase_button_pressed():
-	pass
+	if selected_upgrade.price <= PlayerManager.data.money:
+		PlayerManager.set_upgrade_level(selected_upgrade.type, selected_upgrade.level)
+		PlayerManager.spend_money(selected_upgrade.price)
+		
+		update_locked_and_purchased_items()
 
 func _on_exit_button_pressed():
 	get_tree().paused = false
