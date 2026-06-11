7c28c323d84da49f5632983ec56252469edc280a
SpeedOfSmell <vldmirea@gmail.com>
2026-06-11 08:14:14 -0400

    Add kid in box fort and build dialogue system (#40)

---
+++ assets/game/buying_scene/dialogue/lines/dialogue_line.gd
@@ -0,0 +1,29 @@
+extends Resource
+class_name DialogueLine
+
+@export var text: String
+@export var required_flags: Array[PlayerData.GameEvent] = []    # ALL must be true
+@export var excluded_flags: Array[PlayerData.GameEvent] = []    # NONE must be true
+
+# The text of the button needed clicked to reach this line.
+# A blank value with no sibling dialogue lines will result in no button displayed, just a continue option
+@export var button_text: String 
+# List of possible dialogue lines that can come next
+@export var next_lines: Array[DialogueLine]
+# A list of scripts that will execute when the line is said
+# They need to be of type DialogueScript
+@export var dialogue_scripts: Array[Resource]
+		
+# Used for dynamic checks that cannot be expressed as an event
+@export var extra_condition: DialogueCondition = null
+
+func is_available() -> bool:
+	for flag in required_flags:
+		if not PlayerManager.has_event_triggered(flag):
+			return false
+	for flag in excluded_flags:
+		if PlayerManager.has_event_triggered(flag):
+			return false
+	if extra_condition != null:
+		return extra_condition.evaluate()
+	return true
--- assets/global/player_data.gd
+++ assets/global/player_data.gd
@@ -2,6 +2,16 @@ extends Resource
 
 class_name PlayerData
 
+enum GameEvent { 
+	MET_LOUIS, 
+	FOUND_POLLUTED_FISH, 
+	GAVE_POLLUTED_FISH,
+	FOUND_MAGIC_FISH,
+	GAVE_MAGIC_FISH,
+	BOUGHT_UPGRADE, ## test event,
+	GOT_MONEY_FROM_LOUIS ## test event
+}
+
 @export var fish_inventory: Array[Fish] = []
 @export var money: int = 0
 @export var upgrades: Dictionary = {
@@ -15,3 +25,4 @@ class_name PlayerData
 	Upgrade.UpgradeType.CONSERVATION: 0
 }
 @export var day: int = 1
+@export var events_triggered: Dictionary = {}
