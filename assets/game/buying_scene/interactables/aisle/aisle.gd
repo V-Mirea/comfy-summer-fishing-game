b6c78b14f48bf549d0b44ace70711b634afbe0ae
SpeedOfSmell <vldmirea@gmail.com>
2026-06-13 00:43:46 -0400

    add the rest of the upgrades and shelves

--- assets/game/buying_scene/interactables/aisle/aisle.gd
+++ assets/game/buying_scene/interactables/aisle/aisle.gd
@@ -3,12 +3,24 @@ extends StaticBody2D
 signal shop_opened(upgrades_sold: Upgrade.UpgradeType)
 
 @export var upgrades_sold: Upgrade.UpgradeType
+@export var display_tag_image: Texture2D # this needs to be exported on the aisle so that it can be changed when used in other scenes
+@export var display_tag: Sprite2D
+
+# using this to update the outline shader with the image's transparency value. otherwise it just ignores it
+var _alpha: float = 1.0
+var alpha: float:
+	get: return _alpha
+	set(value):
+		_alpha = value
+		modulate.a = value
+		var mat := $Sprite2D.material as ShaderMaterial #probably bad to reference a child by name but this was annoying to figure out and im tired
+		mat.set_shader_parameter("modulate_color", modulate)
 
 var player_in_range = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	display_tag.texture = display_tag_image
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,8 +42,8 @@ func _on_player_exited_interaction_area():
 
 func _on_transparency_area_body_entered(body):
 	if body is CharacterBody2D:
-		modulate.a = .3
+		alpha = .3
 
 func _on_transparency_area_body_exited(body):
 	if body is CharacterBody2D:
-		modulate.a = 1
+		alpha = 1
